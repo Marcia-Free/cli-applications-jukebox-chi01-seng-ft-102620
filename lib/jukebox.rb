1d1a6f0b68ec724f451d3399b57b3523ef147006
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-24 22:40:05 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,11 +14,11 @@ end
 
 def play(songs_array)
   puts "Please enter a song name or number:"
-  input = gets.strip
+  input = gets.strip.to_s
   
   songs_array.each_with_index do |element, index|
     index += 1
-    if input === index || input == element
+    if input == index || input == element
       puts "Playing #{element}"
     end
   end
