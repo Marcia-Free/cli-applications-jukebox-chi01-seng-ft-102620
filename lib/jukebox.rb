ab497d2b7f6287b8fa52ff988e4ac1ac73038008
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-24 22:09:55 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -4,7 +4,11 @@ end
 
 
 def help
-  
+  puts I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
 end
 
 
